5b4db1c654eab7d918170d268ca6d144c338aa6d
Alisher <fazilov.99@mail.ru>
2020-05-05 22:49:11 +0300

    Update[1]

--- ScriptDataProcessing.py
+++ ScriptDataProcessing.py
@@ -1,24 +1,36 @@
 # %%
 from HDF.HDFData import HDFArray
 path = "./FilteredData/"
+
 # %%
 file = HDFArray(path + '0.h5')
 print(file.ListChannels())
 
 # %%
+fd = file.getChannelTable('FD')[1]
+print(len(fd))
+for i in range(1000):
+    print(fd[i])
+#%%
+
+import matplotlib.pyplot as plt
+plt.plot(fd)
+# %%
 
 print(len(file.getChannelTable('TRACK_MARKERS')[1]))
 print(len(file.getChannelTable('FD')[1]))
+
 # %%
 table_track = file.getChannelTable('TRACK_MARKERS')
 print(table_track)
 print(type(table_track[1]))
 track = table_track[1]
-
+eeg = file.getChannelTable('EEG')
+print(eeg)
 # %%
 print(len(track[0]))
 matrix = track[1]
-for i in range(300):
+for i in range(500):
     print(track[i])
 
 # %%
@@ -41,49 +53,6 @@ for i in range(2, len(track)):
         count += 1
         print("dif = ", stop-start)
         print("time = ", time_stop-time_start)
+print(time_stop, time_start)
+c = 0
 
-print("count = ",count)
-
-# %%
-import pandas as pd
-
-data_frame = pd.DataFrame(array_track, columns = table_track[0])
-print(data_frame.head())
-
-
-# %%
-for i in range(len(array_track)):
-    if (track[i][1] == 0):
-        print(array_track[i])
-    elif (track[i][1] - track[i][1] % 10 == 1000):
-        track_filter.append(array_track[i])
-    elif ()
-
-# %%
-track_filter = []
-flag_move = False
-for i in range(len(array_track)):
-    if (array_track[i][1] == 2):
-        track_filter.append(array_track[i])
-        flag_move = True
-    elif (i != len(array_track) - 1 and array_track[i][1] == 50 and (array_track[i+1][1] != 50)):
-        track_filter.append(array_track[i])
-    elif (array_track[i][1] == 0):
-        track_filter.append(array_track[i])
-    elif (array_track[i][1] == 16000):
-        track_filter.append(array_track[i])
-    elif (array_track[i][1] - array_track[i][1] % 10 == 1000):
-        track_filter.append(array_track[i])
-
-# %%
-for i in track_filter:
-    print(i)
-
-# %%
-print(50 - 50 % 10 == 1000)
-
-# %%
-radius = 5
-print((287-280) / radius)
-
-# %%
